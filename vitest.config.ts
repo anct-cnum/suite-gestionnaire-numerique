7cd3d34580d6906bdc13345b76cc7e0ce939c8a2
Fabien <fabien.mercier@octo.com>
2024-05-27 18:02:57 +0200

    :sparkles: Mise en place du DSFR (#26)

--- vitest.config.ts
+++ vitest.config.ts
@@ -9,6 +9,12 @@ export default defineConfig({
       include: ['src/**/*'],
       provider: 'istanbul',
       skipFull: true,
+      watermarks: {
+        branches: [80, 100],
+        functions: [80, 100],
+        lines: [80, 100],
+        statements: [80, 100],
+      },
     },
     environment: 'jsdom',
     globals: true,
