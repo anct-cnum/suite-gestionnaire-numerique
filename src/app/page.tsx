a16a5c5bed929e7c6d27c3d3813ddbb1cdbfad17
dienamo <dienamoustapha@gmail.com>
2024-05-28 11:03:07 +0200

    :test_tube: configuration de pnpm

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -36,7 +36,7 @@ export default function Home(): ReactElement {
 
       <div className={styles.center}>
         <h1>
-          Suite gestionnaire numérique ! v0.0.7
+          Suite gestionnaire numérique ! v0.0.8
         </h1>
       </div>
 
