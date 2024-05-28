aa5387de69d0b4b6399e49bc1da2c4059534b07f
dienamo <dienamoustapha@gmail.com>
2024-05-28 11:20:32 +0200

    :test_tube: PR de test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -36,7 +36,7 @@ export default function Home(): ReactElement {
 
       <div className={styles.center}>
         <h1>
-          Suite gestionnaire numérique ! v0.0.2
+          Suite gestionnaire numérique ! v0.0.20
         </h1>
       </div>
 
