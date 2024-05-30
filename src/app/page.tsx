cb097849c26e62f39ea5a6e5749bd4bd0f7824bf
dienamo <dienamoustapha@gmail.com>
2024-05-30 18:32:11 +0200

    :test_tube: mise à jour manuelle ci

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -36,7 +36,7 @@ export default function Home(): ReactElement {
 
       <div className={styles.center}>
         <h1>
-          Suite gestionnaire numérique ! v0.2.0
+          Suite gestionnaire numérique ! v0.8.0
         </h1>
       </div>
 
