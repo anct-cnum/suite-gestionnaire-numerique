17c4199dcb5a27fe2d22da312dac7c889405885e
Fabien <fabien.mercier@octo.com>
2024-05-31 10:59:22 +0200

    :sparkles: Installation et configuration de Sentry (#40)

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,5 @@
+import { withSentryConfig } from '@sentry/nextjs'
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
@@ -6,7 +8,6 @@ const nextConfig = {
   experimental: {
     typedRoutes: true,
   },
-  output: 'export',
   poweredByHeader: false,
   reactStrictMode: true,
   typescript: {
@@ -14,4 +15,11 @@ const nextConfig = {
   },
 }
 
-export default nextConfig
+const SentryBuildOptions = {
+  disableLogger: true,
+  hideSourceMaps: true,
+  silent: true,
+  widenClientFileUpload: true,
+}
+
+export default withSentryConfig(nextConfig, SentryBuildOptions)
--- src/app/global-error.tsx
+++ src/app/global-error.tsx
@@ -1,14 +1,17 @@
 'use client'
 
-import { ReactElement } from 'react'
+import { captureException } from '@sentry/nextjs'
+import Error from 'next/error'
+import { ReactElement, useEffect } from 'react'
 
 type ErrorProps = Readonly<{
   error: Readonly<Error>
 }>
 
 export default function GlobalError({ error }: ErrorProps): ReactElement {
-  // eslint-disable-next-line no-console
-  console.error(error)
+  useEffect(() => {
+    captureException(error)
+  }, [error])
 
   return (
     <html
