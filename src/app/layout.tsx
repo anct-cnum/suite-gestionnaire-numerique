f41f83350d18b70ce9beadd04f5c7342bbe22690
Fabien Mercier <fabien.mercier@octo.com>
2024-07-29 15:00:09 +0200

    :sparkles: Utilisation du .module.js du DSFR au lieu du nomodule.js

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,7 @@ export default function Layout({ children }: PropsWithChildren): ReactElement {
           type="image/x-icon"
         />
         <Script
-          src="/dsfr-1.5.0.nomodule.min.js"
+          src="/dsfr-1.12.1.module.min.js"
           strategy="lazyOnload"
         />
       </head>
