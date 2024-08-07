41af9a27dbf4100bc1cff539305f15646855e7f1
Fabien <fabien.mercier@octo.com>
2024-08-07 16:10:48 +0200

    :sparkles: Cliquer sur le backdrop pour fermer le menu utilisateur (#86)
    * :sparkles: Cliquer sur le backdrop pour fermer le menu utilisateur

--- src/components/shared/Drawer/Drawer.tsx
+++ src/components/shared/Drawer/Drawer.tsx
@@ -17,20 +17,22 @@ export default function Drawer({
       id={id}
       open={isOpen}
     >
-      <div className="fr-grid-row fr-grid-row--right">
-        <div className={`fr-col-4 ${styles['modal-box']} ${isFixedWidth ? styles['modal-box--fixed-width'] : ''}`}>
-          <div className={`fr-modal__body ${styles['fr-modal__body']}`}>
-            <button
-              aria-controls={id}
-              className={`fr-btn fr-btn--tertiary fr-icon-close-line ${styles.close}`}
-              onClick={() => {
-                setIsOpen(false)
-              }}
-              title={boutonFermeture}
-              type="button"
-            />
-            <div className={`fr-modal__content ${styles['fr-modal__content']}`}>
-              {children}
+      <div className={`fr-container ${styles['fr-container']}`}>
+        <div className="fr-grid-row fr-grid-row--right">
+          <div className={`fr-col-4 ${styles['modal-box']} ${isFixedWidth ? styles['modal-box--fixed-width'] : ''}`}>
+            <div className={`fr-modal__body ${styles['fr-modal__body']}`}>
+              <button
+                aria-controls={id}
+                className={`fr-btn fr-btn--tertiary fr-icon-close-line ${styles.close}`}
+                onClick={() => {
+                  setIsOpen(false)
+                }}
+                title={boutonFermeture}
+                type="button"
+              />
+              <div className={`fr-modal__content ${styles['fr-modal__content']}`}>
+                {children}
+              </div>
             </div>
           </div>
         </div>
