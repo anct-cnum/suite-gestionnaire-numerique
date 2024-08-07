9ec467a4d8efc7923c28cf799cdfd34f95ff1a7c
Fabien Mercier <fabien.mercier@octo.com>
2024-08-07 15:40:38 +0200

    :sparkles: Migration des utilisateurs de mongodb et FNE vers la suite gestionnaire

---
+++ prisma/prismaClient.ts
@@ -0,0 +1,46 @@
+import { Prisma, PrismaClient } from '@prisma/client'
+
+declare const globalThis: typeof global & {
+  prismaGlobal: ReturnType<typeof prismaClientSingleton>;
+}
+
+// eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+const prisma = globalThis.prismaGlobal ?? prismaClientSingleton()
+
+export default prisma
+
+if (process.env.NODE_ENV !== 'production') {
+  globalThis.prismaGlobal = prisma
+}
+
+function prismaClientSingleton(): PrismaClient<Prisma.PrismaClientOptions, never> {
+  const prisma = new PrismaClient({
+    log: [
+      {
+        emit: 'event',
+        level: 'query',
+      },
+      {
+        emit: 'stdout',
+        level: 'error',
+      },
+      {
+        emit: 'stdout',
+        level: 'info',
+      },
+      {
+        emit: 'stdout',
+        level: 'warn',
+      },
+    ],
+  })
+
+  // A décommenter pour déboguer Prisma
+  // prisma.$on('query', (eventType) => {
+  //   console.log('Query: ' + eventType.query)
+  //   console.log('Params: ' + eventType.params)
+  //   console.log('Duration: ' + eventType.duration + 'ms')
+  // })
+
+  return prisma
+}
