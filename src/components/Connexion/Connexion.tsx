e9f0033e1dccea988e2083b108123b2b8623d3bb
Fabien Mercier <fabien.mercier@octo.com>
2024-08-15 11:37:09 +0200

    :sparkles: Amélioration de la page de connexion

--- src/components/Connexion/Connexion.tsx
+++ src/components/Connexion/Connexion.tsx
@@ -12,15 +12,39 @@ type ConnexionProps = Readonly<{
 
 export default function Connexion({ provider }: ConnexionProps): ReactElement {
   return (
-    <div className="fr-grid-row fr-grid-row--center">
-      <button
-        className="btn btn--plain btn--primary"
-        onClick={async () => signIn(provider.id, { callbackUrl: '/' })}
-        title="S’identifier avec ProConnect"
-        type="button"
-      >
-        <ProConnect />
-      </button>
+    <div className="fr-container fr-container--fluid">
+      <div className="fr-grid-row fr-grid-row-gutters fr-grid-row--center">
+        <div className="fr-col-12 fr-col-md-8 fr-col-lg-6">
+          <h1>
+            Connexion à la suite gestionnaire numérique
+          </h1>
+          <div className="fr-mb-6v">
+            <h2>
+              Se connecter avec ProConnect
+            </h2>
+            <div className="fr-connect-group">
+              <button
+                className="btn btn--plain btn--primary"
+                onClick={async () => signIn(provider.id, { callbackUrl: '/' })}
+                title="S’identifier avec ProConnect"
+                type="button"
+              >
+                <ProConnect />
+              </button>
+              <p>
+                <a
+                  href="/"
+                  rel="external noopener noreferrer"
+                  target="_blank"
+                  title="Qu’est ce que ProConnect ? (nouvelle fenêtre)"
+                >
+                  Qu’est ce que ProConnect ?
+                </a>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
