5c7659fc1870def258a823b201b538303c0c717b
Mehdi Ould-Hamouda <mehdi.ould.hamouda@octo.com>
2024-09-04 17:34:13 +0200

    chore(Prisma): don't log caught error messages in test environment

--- prisma/prismaClient.ts
+++ prisma/prismaClient.ts
@@ -14,26 +14,29 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 function prismaClientSingleton(): PrismaClient<Prisma.PrismaClientOptions, never> {
-  const prisma = new PrismaClient({
-    log: [
-      {
-        emit: 'event',
-        level: 'query',
-      },
-      {
-        emit: 'stdout',
-        level: 'error',
-      },
-      {
-        emit: 'stdout',
-        level: 'info',
-      },
-      {
-        emit: 'stdout',
-        level: 'warn',
-      },
-    ],
-  })
+  const prisma = new PrismaClient(
+    {
+      log: process.env.NODE_ENV === 'test'
+        ? []
+        : [
+        {
+          emit: 'event',
+          level: 'query',
+        },
+        {
+          emit: 'stdout',
+          level: 'error',
+        },
+        {
+          emit: 'stdout',
+          level: 'info',
+        },
+        {
+          emit: 'stdout',
+          level: 'warn',
+        },
+      ],
+    })
 
   // A décommenter pour déboguer Prisma
   // prisma.$on('query', (eventType) => {
