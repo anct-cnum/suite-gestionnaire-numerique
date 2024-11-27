7bf5e738a5998228636e200594b3393680b98494
Fabien Mercier <fabien.mercier@octo.com>
2024-11-27 09:50:26 +0100

    Mise en place d'une gouvernance remplie avec des données bouchonnées côté back #166

--- src/use-cases/queries/RecupererUneGouvernance.ts
+++ src/use-cases/queries/RecupererUneGouvernance.ts
@@ -3,5 +3,26 @@ export interface UneGouvernanceReadModelLoader {
 }
 
 export type UneGouvernanceReadModel = Readonly<{
-  nom: string
+  departement: string
+  comites?: ReadonlyArray<{
+    dateProchainComite: Date
+    nom: string
+    periodicite: string
+  }>
+  feuillesDeRoute?: ReadonlyArray<{
+    budgetGlobal: number
+    nom: string
+    totalActions: number
+  }>
+  membres?: ReadonlyArray<{
+    nom: string
+    roles: ReadonlyArray<string>
+    type: string
+  }>
+  noteDeContexte?: Readonly<{
+    dateDeModification: Date
+    nomAuteur: string
+    prenomAuteur: string
+    texte: string
+  }>
 }>
