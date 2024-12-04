e5583b38e0cb323f702a6861daf8af83c4bfd606
ornella <ornella93@outlook.fr>
2024-12-04 14:22:42 +0100

    Ajout du sous menu Gouvernance : Membres, Feuilles de route

--- src/components/transverse/MenuLateral/MenuLateral.test.tsx
+++ src/components/transverse/MenuLateral/MenuLateral.test.tsx
@@ -1,4 +1,4 @@
-import { screen, within } from '@testing-library/react'
+import { fireEvent, screen, within } from '@testing-library/react'
 
 import MenuLateral from './MenuLateral'
 import { renderComponent } from '@/components/testHelper'
@@ -28,10 +28,31 @@ describe('menu lateral', () => {
 
     // THEN
     const navigation = screen.getByRole('navigation', { name: 'Menu inclusion numérique' })
-    const menu = within(navigation).getByRole('list')
-    const menuItems = within(menu).getAllByRole('listitem')
+    const menuItems = within(navigation).getAllByRole('listitem')
+    expect(menuItems).toHaveLength(5)
     const tableauDeBord = within(menuItems[1]).getByRole('link', { name: 'Gouvernance' })
     expect(tableauDeBord).toHaveAttribute('href', '/gouvernance/93')
+  })
+
+  it('étant un gestionnaire de département, quand je clique sur menu Gouvernance, alors 2 sous menus s’affiche', () => {
+    // WHEN
+    renderComponent(<MenuLateral />, {
+      sessionUtilisateurViewModel: sessionUtilisateurViewModelFactory({
+        codeDepartement: '93',
+        displayLiensGouvernance: true,
+      }),
+    })
+
+    // THEN
+    const navigation = screen.getByRole('navigation', { name: 'Menu inclusion numérique' })
+    const menus = within(navigation).getAllByRole('listitem')
+    expect(menus).toHaveLength(5)
+    const menuGouvernance = within(menus[1]).getByRole('link', { name: 'Gouvernance' })
+    fireEvent.click(menuGouvernance)
+    const sousMenuMembres = screen.getByRole('link', { name: 'Membres' })
+    expect (sousMenuMembres).toBeInTheDocument()
+    const sousMenuFeuillesDeRoute = screen.getByRole('link', { name: 'Feuilles de route' })
+    expect (sousMenuFeuillesDeRoute).toBeInTheDocument()
   })
 
   it('étant un utilisateur autre que gestionnaire de département, quand j’affiche le menu latéral, alors il ne s’affiche pas avec le lien de la gouvernance', () => {
