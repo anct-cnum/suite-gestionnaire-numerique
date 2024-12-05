36cd3844e42151ece32e123a52a8efcb81bbbc1b
Mehdi Ould-Hamouda <mehdi.ould.hamouda@octo.com>
2024-12-05 09:15:37 +0100

    suite du test

--- src/use-cases/commands/AjouterNoteDeContexteAGouvernance.ts
+++ src/use-cases/commands/AjouterNoteDeContexteAGouvernance.ts
@@ -1,4 +1,4 @@
-import { Either, MaybeAsync } from 'purify-ts'
+import { Either, Left, Maybe, Right } from 'purify-ts'
 
 import { FindGouvernanceRepository, UpdateGouvernanceRepository } from './shared/GouvernanceRepository'
 import { FindUtilisateurRepository } from './shared/UtilisateurRepository'
@@ -21,28 +21,56 @@ export class AjouterNoteDeContexteAGouvernance {
   }
 
   async execute({ contenu, uid, uidGouvernance }: Command): Promise<Either<Failure, 'Ok'>> {
-    return MaybeAsync
-      .fromPromise(async () => this.#gouvernanceRepository.find(new GouvernanceUid(uidGouvernance)))
-      .map(async (gouvernance) => {
-        gouvernance.ajouterNoteDeContexte(new NoteDeContexte(this.#date, new UtilisateurUid({ email: 'eme', value: uid }), contenu))
+    // VERSION ENTIÈREMENT CHAÎNÉE
+    // return MaybeAsync.liftMaybe(Maybe.fromNullable(await this.#utilisateurRepository.find(uid)))
+    //   .toEitherAsync('utilisateurInexistant' as const)
+    //   .chain(utilisateurCourant => MaybeAsync
+    //     .fromPromise(async () => this.#gouvernanceRepository.find(new GouvernanceUid(uidGouvernance)))
+    //     .map(async (gouvernance) => {
+    //       gouvernance.ajouterNoteDeContexte(
+    //         new NoteDeContexte(this.#date, new UtilisateurUid(utilisateurCourant!.state.uid), contenu)
+    //       )
+    //       await this.#gouvernanceRepository.update(gouvernance)
+    //       return 'Ok' as const
+    //     })
+    //     .toEitherAsync('gouvernanceInexistante' as const)
+    //   )
 
-        await this.#gouvernanceRepository.update(gouvernance)
-
-        return 'Ok' as const
-      })
-      .toEitherAsync('gouvernanceInexistante')
-    // const t = async (params: MaybeAsyncHelpers): Promise<'Ok'> => {
-    //   const gouvernance = await params.fromPromise(this.#repository.find(new GouvernanceUid(uidGouvernance)))
-    //   gouvernance.ajouterNoteDeContexte(contenu)
-    //   await this.#repository.update(gouvernance)
+    // VERSION IMPÉRATIVE MAIS AVEC CALLBACK
+    // return EitherAsync(async (helpers) => {
+    //   const maybeUtilisateurCourant = Maybe.fromNullable(await this.#utilisateurRepository.find(uid))
+    //   const utilisateurCourant = await helpers.liftEither(maybeUtilisateurCourant.toEither('utilisateurInexistant'))
+    //   const maybeGouvernance = (await this.#gouvernanceRepository.find(new GouvernanceUid(uidGouvernance)))
+    //   const gouvernance = await helpers.liftEither(maybeGouvernance.toEither('gouvernanceInexistante' as const))
+    //   gouvernance.ajouterNoteDeContexte(
+    //     new NoteDeContexte(this.#date, new UtilisateurUid(utilisateurCourant.state.uid), contenu)
+    //   )
+    //   await this.#gouvernanceRepository.update(gouvernance)
     //   return 'Ok' as const
-    // }
+    // })
 
-    // return MaybeAsync(t).toEitherAsync('gouvernanceInexistante')
+    // VERSION FULL À LA ZOB (COMME AVEC LES RESULT)
+    const maybeUtilisateurCourant = Maybe.fromNullable(await this.#utilisateurRepository.find(uid))
+    if (maybeUtilisateurCourant.isNothing()) {
+      return Left('utilisateurInexistant')
+    }
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    const utilisateurCourant = maybeUtilisateurCourant.extract()!
+    const maybeGouvernance = await this.#gouvernanceRepository.find(new GouvernanceUid(uidGouvernance))
+    if (maybeGouvernance.isNothing()) {
+      return Left('gouvernanceInexistante')
+    }
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    const gouvernance = maybeGouvernance.extract()!
+    gouvernance.ajouterNoteDeContexte(
+      new NoteDeContexte(this.#date, new UtilisateurUid(utilisateurCourant.state.uid), contenu)
+    )
+    await this.#gouvernanceRepository.update(gouvernance)
+    return Right('Ok')
   }
 }
 
-type Failure = 'gouvernanceInexistante'
+type Failure = 'gouvernanceInexistante' | 'utilisateurInexistant'
 
 type Command = Readonly<{
   contenu: string
