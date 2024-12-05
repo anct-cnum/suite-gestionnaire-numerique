c4e098848a3aa87c21da3e9a6667ec8502a2c7e2
Mehdi Ould-Hamouda <mehdi.ould.hamouda@octo.com>
2024-12-05 09:37:46 +0100

    suppression de purify-ts, utilisation de Rsult/ResultAsync au lieu de Either

--- src/use-cases/commands/AjouterNoteDeContexteAGouvernance.ts
+++ src/use-cases/commands/AjouterNoteDeContexteAGouvernance.ts
@@ -1,11 +1,10 @@
-import { Either, Left, Maybe, Right } from 'purify-ts'
-
+import { CommandHandler, ResultAsync } from '../CommandHandler'
 import { FindGouvernanceRepository, UpdateGouvernanceRepository } from './shared/GouvernanceRepository'
 import { FindUtilisateurRepository } from './shared/UtilisateurRepository'
 import { GouvernanceUid, NoteDeContexte } from '@/domain/Gouvernance'
 import { UtilisateurUid } from '@/domain/Utilisateur'
 
-export class AjouterNoteDeContexteAGouvernance {
+export class AjouterNoteDeContexteAGouvernance implements CommandHandler<Command> {
   readonly #gouvernanceRepository: GouvernanceRepository
   readonly #utilisateurRepository: UtilisateurRepository
   readonly #date: Date
@@ -20,53 +19,20 @@ export class AjouterNoteDeContexteAGouvernance {
     this.#date = date
   }
 
-  async execute({ contenu, uid, uidGouvernance }: Command): Promise<Either<Failure, 'Ok'>> {
-    // VERSION ENTIÈREMENT CHAÎNÉE
-    // return MaybeAsync.liftMaybe(Maybe.fromNullable(await this.#utilisateurRepository.find(uid)))
-    //   .toEitherAsync('utilisateurInexistant' as const)
-    //   .chain(utilisateurCourant => MaybeAsync
-    //     .fromPromise(async () => this.#gouvernanceRepository.find(new GouvernanceUid(uidGouvernance)))
-    //     .map(async (gouvernance) => {
-    //       gouvernance.ajouterNoteDeContexte(
-    //         new NoteDeContexte(this.#date, new UtilisateurUid(utilisateurCourant!.state.uid), contenu)
-    //       )
-    //       await this.#gouvernanceRepository.update(gouvernance)
-    //       return 'Ok' as const
-    //     })
-    //     .toEitherAsync('gouvernanceInexistante' as const)
-    //   )
-
-    // VERSION IMPÉRATIVE MAIS AVEC CALLBACK
-    // return EitherAsync(async (helpers) => {
-    //   const maybeUtilisateurCourant = Maybe.fromNullable(await this.#utilisateurRepository.find(uid))
-    //   const utilisateurCourant = await helpers.liftEither(maybeUtilisateurCourant.toEither('utilisateurInexistant'))
-    //   const maybeGouvernance = (await this.#gouvernanceRepository.find(new GouvernanceUid(uidGouvernance)))
-    //   const gouvernance = await helpers.liftEither(maybeGouvernance.toEither('gouvernanceInexistante' as const))
-    //   gouvernance.ajouterNoteDeContexte(
-    //     new NoteDeContexte(this.#date, new UtilisateurUid(utilisateurCourant.state.uid), contenu)
-    //   )
-    //   await this.#gouvernanceRepository.update(gouvernance)
-    //   return 'Ok' as const
-    // })
-
-    // VERSION FULL À LA ZOB (COMME AVEC LES RESULT)
-    const maybeUtilisateurCourant = Maybe.fromNullable(await this.#utilisateurRepository.find(uid))
-    if (maybeUtilisateurCourant.isNothing()) {
-      return Left('utilisateurInexistant')
+  async execute({ contenu, uid, uidGouvernance }: Command): ResultAsync<Failure> {
+    const utilisateurCourant = await this.#utilisateurRepository.find(uid)
+    if (!utilisateurCourant) {
+      return 'utilisateurInexistant'
     }
-    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-    const utilisateurCourant = maybeUtilisateurCourant.extract()!
-    const maybeGouvernance = await this.#gouvernanceRepository.find(new GouvernanceUid(uidGouvernance))
-    if (maybeGouvernance.isNothing()) {
-      return Left('gouvernanceInexistante')
+    const gouvernance = await this.#gouvernanceRepository.find(new GouvernanceUid(uidGouvernance))
+    if (!gouvernance) {
+      return 'gouvernanceInexistante'
     }
-    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-    const gouvernance = maybeGouvernance.extract()!
     gouvernance.ajouterNoteDeContexte(
       new NoteDeContexte(this.#date, new UtilisateurUid(utilisateurCourant.state.uid), contenu)
     )
     await this.#gouvernanceRepository.update(gouvernance)
-    return Right('Ok')
+    return 'OK'
   }
 }
 
@@ -79,4 +45,5 @@ type Command = Readonly<{
 }>
 
 interface GouvernanceRepository extends FindGouvernanceRepository, UpdateGouvernanceRepository {}
+
 type UtilisateurRepository = FindUtilisateurRepository
