adb42ca87db00d50374e625eb541690a5ae2dd63
ornella <ornella93@outlook.fr>
2024-12-18 16:08:12 +0100

    ajout du drawer pour les membres

---
+++ src/components/Gouvernance/Membre/DetailsMembre.tsx
@@ -0,0 +1,16 @@
+import { ReactElement } from 'react'
+
+import DrawerTitle from '@/components/shared/DrawerTitle/DrawerTitle'
+import { MembreViewModel } from '@/presenters/gouvernancePresenter'
+
+export default function DetailsMembre({ membreDetails, labelId }: Props): ReactElement {
+  return (
+    <DrawerTitle id={labelId}>
+      {membreDetails.nom}
+    </DrawerTitle>
+  )
+}
+type Props = Readonly<{
+  membreDetails: MembreViewModel,
+  labelId: string
+}>
