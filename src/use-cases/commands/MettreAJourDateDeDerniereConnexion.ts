eed9953d22afebaabd347b9fb43b10b6b204e7f6
dienamo <46058257+dienamo@users.noreply.github.com>
2024-12-19 09:11:01 +0100

    configuration de la date de dernère connexion et exemple d'utilisatio… (#257)
    * configuration de la date de dernère connexion et exemple d'utilisation de la fonction de callback au lieu du layout à la connexion
    
    * appel du use-case dans le callback signIn
    
    * traitements retours commentaires
    
    * configuration erreur date dans le domaine
    
    ---------
    
    Co-authored-by: Mo <admin@MacBook-Pro-de-Mo.local>

---
+++ src/use-cases/commands/MettreAJourDateDeDerniereConnexion.ts
@@ -0,0 +1,32 @@
+import { CommandHandler, ResultAsync } from '../CommandHandler'
+import { FindUtilisateurRepository, UpdateUtilisateurRepository } from './shared/UtilisateurRepository'
+import { UtilisateurFailure } from '@/domain/Utilisateur'
+
+export class MettreAJourDateDeDerniereConnexion implements CommandHandler<Command> {
+  readonly #utilisateurRepository: UtilisateurRepository
+  readonly #date: Date
+
+  constructor(utilisateurRepository: UtilisateurRepository, date: Date = new Date()) {
+    this.#utilisateurRepository = utilisateurRepository
+    this.#date = date
+  }
+
+  async execute(command: Command): ResultAsync<Failure> {
+    const utilisateurCourant = await this.#utilisateurRepository.find(command.uidUtilisateurCourant)
+    if (!utilisateurCourant) {
+      return 'utilisateurCourantInexistant'
+    }
+
+    utilisateurCourant.changerDateDeDerniereConnexion(this.#date)
+    await this.#utilisateurRepository.update(utilisateurCourant)
+    return 'OK'
+  }
+}
+
+type Failure = 'utilisateurCourantInexistant' | 'dateInvalide' | UtilisateurFailure
+
+type Command = Readonly<{
+  uidUtilisateurCourant: string
+}>
+
+interface UtilisateurRepository extends FindUtilisateurRepository, UpdateUtilisateurRepository {}
