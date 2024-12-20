57a6f4b883e93e9c9975ff8f31d20419892e01d5
ornella <ornella93@outlook.fr>
2024-12-20 15:45:31 +0100

    resolve mutant

--- src/use-cases/queries/RecupererUneGouvernance.ts
+++ src/use-cases/queries/RecupererUneGouvernance.ts
@@ -52,9 +52,9 @@ export type MembreDetailsReadModel = Readonly<{
   contactPolitique: string,
   telephone?: string,
   typologieMembre: string,
-  feuillesDeRoute?: ReadonlyArray<Readonly<{
+  feuillesDeRoute: ReadonlyArray<Readonly<{
     nom: string
-  }>> | undefined
+  }>>
 }>
 
 export type TypeDeComite = 'stratégique' | 'technique' | 'consultatif' | 'autre'
