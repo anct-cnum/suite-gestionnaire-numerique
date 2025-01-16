c1c720f1dd28d914a7836ad8a4b9a00e7b606901
Fabien Mercier <fabien.mercier@octo.com>
2025-01-16 14:30:47 +0100

    Réglage du scroll vertical sur le drawer

--- src/components/Gouvernance/Membre/Membre.tsx
+++ src/components/Gouvernance/Membre/Membre.tsx
@@ -1,11 +1,14 @@
 import { ReactElement } from 'react'
 
 import DrawerTitle from '@/components/shared/DrawerTitle/DrawerTitle'
+import Icon from '@/components/shared/Icon/Icon'
 import { MembreViewModel } from '@/presenters/gouvernancePresenter'
 
 export default function DetailsMembre({ membreDetails, labelId }: Props): ReactElement {
   return (
     <DrawerTitle id={labelId}>
+      <Icon icon={membreDetails.logo} />
+      <br />
       {membreDetails.nom}
     </DrawerTitle>
   )
