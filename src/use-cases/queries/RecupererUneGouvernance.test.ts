2bd67520f0ffac609c41e54833a7ae2717c55eae
ornella <ornella93@outlook.fr>
2025-01-20 13:33:09 +0100

    uses cases details membres

---
+++ src/use-cases/queries/RecupererUneGouvernance.test.ts
@@ -0,0 +1,185 @@
+import { UneGouvernanceReadModelLoader, RecupererUneGouvernance, UneGouvernanceReadModel } from './RecupererUneGouvernance'
+import { gouvernanceReadModelFactory } from '../testHelper'
+
+describe('recupererUneGouvernance', () => {
+  afterEach(() => {
+    uneGouvernance = gouvernanceReadModelFactory()
+  })
+
+  it("quand une gouvernance est demandée sur un département et qu'elle n'existe pas alors on récupère une gouvernance vide", async () => {
+    // GIVEN
+
+    const queryHandler = new RecupererUneGouvernance(new GouvernanceInexistanteLoaderStub())
+
+    // WHEN
+    const gouvernance = await queryHandler.get({ codeDepartement: '69' })
+
+    // THEN
+    expect(gouvernance).toStrictEqual({ departement: 'Rhône', uid: 'gouvernanceFooId' })
+  })
+
+  it("quand une gouvernance est demandée sur un département et qu'elle existe alors on la récupère en calculant les totaux des montants de subvention", async () => {
+    // GIVEN
+    const queryHandler = new RecupererUneGouvernance(new GouvernanceExistanteLoaderStub())
+
+    // WHEN
+    const gouvernance = await queryHandler.get({ codeDepartement: '69' })
+
+    // THEN
+    expect(gouvernance).toStrictEqual(gouvernanceEnrichie)
+  })
+
+  it("quand une gouvernance sans membre est demandée sur un département et qu'elle existe alors on la récupère", async () => {
+    // GIVEN
+    uneGouvernance = {
+      ...uneGouvernance,
+      membres: [],
+    }
+    const queryHandler = new RecupererUneGouvernance(new GouvernanceExistanteLoaderStub())
+
+    // WHEN
+    const gouvernance = await queryHandler.get({ codeDepartement: '69' })
+
+    // THEN
+    expect(gouvernance).toStrictEqual(gouvernanceSansMembre)
+  })
+
+  it("quand une gouvernance est demandée sur un département et qu'elle existe, contenant 1 membre préfecture departementale, alors on la récupère sans les totaux subventions et sans links mais avec le contact technique", async () => {
+    // GIVEN
+    uneGouvernance = {
+      ...uneGouvernance,
+      membres: [
+        {
+          contactReferent: {
+            denomination: 'Contact politique de la collectivité',
+            mailContact: 'julien.deschamps@rhones.gouv.fr',
+            nom: 'Henrich',
+            poste: 'chargé de mission',
+            prenom: 'Laetitia',
+          },
+          contactTechnique: 'Simon.lagrange@rhones.gouv.fr',
+          feuillesDeRoute: [{ montantSubventionAccorde: 5_000, montantSubventionFormationAccorde: 5_000, nom: 'Feuille de route inclusion' }, { montantSubventionAccorde: 5_000, montantSubventionFormationAccorde: 5_000, nom: 'Feuille de route numérique du Rhône' }],
+          links: {},
+          nom: 'Préfecture du Rhône',
+          roles: ['Co-porteur'],
+          telephone: '+33 4 45 00 45 00',
+          type: 'Administration',
+          typologieMembre: 'Préfecture départementale',
+        },
+      ],
+    }
+    const queryHandler = new RecupererUneGouvernance(new GouvernanceExistanteLoaderStub())
+
+    // WHEN
+    const gouvernance = await queryHandler.get({ codeDepartement: '69' })
+
+    // THEN
+    expect(gouvernance.membres).toStrictEqual([
+      {
+        contactReferent: {
+          denomination: 'Contact politique de la collectivité',
+          mailContact: 'julien.deschamps@rhones.gouv.fr',
+          nom: 'Henrich',
+          poste: 'chargé de mission',
+          prenom: 'Laetitia',
+        },
+        contactTechnique: 'Simon.lagrange@rhones.gouv.fr',
+        feuillesDeRoute: [{ montantSubventionAccorde: 5_000, montantSubventionFormationAccorde: 5_000, nom: 'Feuille de route inclusion' }, { montantSubventionAccorde: 5_000, montantSubventionFormationAccorde: 5_000, nom: 'Feuille de route numérique du Rhône' }],
+        links: { },
+        nom: 'Préfecture du Rhône',
+        roles: ['Co-porteur'],
+        telephone: '+33 4 45 00 45 00',
+        type: 'Administration',
+        typologieMembre: 'Préfecture départementale',
+      },
+    ])
+  })
+
+  it("quand une gouvernance est demandée sur un département et qu'elle existe, contenant 1 membre autre que la prefecture departementale, alors on la récupère avec les totaux subventions et le lien mais sans le contact technique", async () => {
+    // GIVEN
+    uneGouvernance = {
+      ...uneGouvernance,
+      membres: [
+        {
+          contactReferent: {
+            denomination: 'Contact référent',
+            mailContact: 'didier.durand@exemple.com',
+            nom: 'Didier',
+            poste: 'chargé de mission',
+            prenom: 'Durant',
+          },
+          feuillesDeRoute: [{ montantSubventionAccorde: 5_000, montantSubventionFormationAccorde: 5_000, nom: 'Feuille de route inclusion' }],
+          links: {},
+          nom: 'Département du Rhône',
+          roles: ['Co-porteur', 'Financeur'],
+          telephone: '+33 4 45 00 45 01',
+          totalMontantSubventionAccorde: 0,
+          totalMontantSubventionFormationAccorde: 0,
+          type: 'Collectivité',
+          typologieMembre: 'Collectivité, EPCI',
+        },
+      ],
+    }
+    const queryHandler = new RecupererUneGouvernance(new GouvernanceExistanteLoaderStub())
+
+    // WHEN
+    const gouvernance = await queryHandler.get({ codeDepartement: '69' })
+
+    // THEN
+    expect(gouvernance.membres).toStrictEqual([
+      {
+        contactReferent: {
+          denomination: 'Contact référent',
+          mailContact: 'didier.durand@exemple.com',
+          nom: 'Didier',
+          poste: 'chargé de mission',
+          prenom: 'Durant',
+        },
+        feuillesDeRoute: [{ montantSubventionAccorde: 5_000, montantSubventionFormationAccorde: 5_000, nom: 'Feuille de route inclusion' }],
+        links: {
+          plusDetails: '/',
+        },
+        nom: 'Département du Rhône',
+        roles: ['Co-porteur', 'Financeur'],
+        telephone: '+33 4 45 00 45 01',
+        totalMontantSubventionAccorde: 5000,
+        totalMontantSubventionFormationAccorde: 5000,
+        type: 'Collectivité',
+        typologieMembre: 'Collectivité, EPCI',
+      },
+    ])
+  })
+})
+
+let uneGouvernance: UneGouvernanceReadModel = gouvernanceReadModelFactory()
+
+const gouvernanceEnrichie: UneGouvernanceReadModel = {
+  ...uneGouvernance,
+  ...uneGouvernance.membres && {
+    membres: [
+      uneGouvernance.membres[0],
+      {
+        ...uneGouvernance.membres[1],
+        totalMontantSubventionAccorde: 5_000,
+        totalMontantSubventionFormationAccorde: 5_000,
+      },
+    ],
+  },
+}
+
+const gouvernanceSansMembre: UneGouvernanceReadModel = {
+  ...uneGouvernance,
+  membres: [],
+}
+
+class GouvernanceInexistanteLoaderStub extends UneGouvernanceReadModelLoader {
+  protected override async find(): Promise<UneGouvernanceReadModel> {
+    return Promise.resolve({ departement: uneGouvernance.departement, uid: uneGouvernance.uid })
+  }
+}
+
+class GouvernanceExistanteLoaderStub extends UneGouvernanceReadModelLoader {
+  protected async find(): Promise<UneGouvernanceReadModel> {
+    return Promise.resolve(uneGouvernance)
+  }
+}
--- src/use-cases/queries/RecupererUneGouvernance.ts
+++ src/use-cases/queries/RecupererUneGouvernance.ts
@@ -1,21 +1,44 @@
-export interface UneGouvernanceReadModelLoader {
-  find(codeDepartement: string): Promise<UneGouvernanceReadModel | null>
+import { QueryHandler } from '../QueryHandler'
+import { identity, UnaryOperator } from '@/shared/lang'
+
+export class RecupererUneGouvernance implements QueryHandler<Query, UneGouvernanceReadModel> {
+  readonly #loader: UneGouvernanceReadModelLoader
+
+  constructor(loader: UneGouvernanceReadModelLoader) {
+    this.#loader = loader
+  }
+
+  async get({ codeDepartement }: Query): Promise<UneGouvernanceReadModel> {
+    return this.#loader.trouverEtEnrichir(codeDepartement, (gouvernance) => ({
+      ...gouvernance,
+      ...gouvernance.membres && {
+        membres: gouvernance.membres.values()
+          .map(toMembreDetailAvecTotauxReadModel)
+          .map(toMembreDetailIntitulerReadModel)
+          .toArray(),
+      },
+    }))
+  }
+}
+
+export abstract class UneGouvernanceReadModelLoader {
+  async trouverEtEnrichir(
+    codeDepartement: string,
+    enrichir: UnaryOperator<UneGouvernanceReadModel> = identity
+  ): Promise<UneGouvernanceReadModel> {
+    return this.find(codeDepartement).then(enrichir)
+  }
+
+  protected abstract find(codeDepartement: string): Promise<UneGouvernanceReadModel>
 }
 
 export type UneGouvernanceReadModel = Readonly<{
   departement: string
   comites?: ReadonlyArray<ComiteReadModel>
   feuillesDeRoute?: ReadonlyArray<FeuilleDeRouteReadModel>
-  membres?: ReadonlyArray<MembreReadModel>
+  membres?: ReadonlyArray<MembreDetailReadModel>
   noteDeContexte?: NoteDeContexteReadModel
   uid: string
-}>
-
-type NoteDeContexteReadModel = Readonly<{
-  dateDeModification: Date
-  nomAuteur: string
-  prenomAuteur: string
-  texte: string
 }>
 
 export type ComiteReadModel = Readonly<{
@@ -45,3 +68,68 @@ export type MembreReadModel = Readonly<{
 }>
 
 export type TypeDeComite = 'stratégique' | 'technique' | 'consultatif' | 'autre'
+
+type NoteDeContexteReadModel = Readonly<{
+  dateDeModification: Date
+  nomAuteur: string
+  prenomAuteur: string
+  texte: string
+}>
+export type MembreDetailReadModel = Readonly<{
+  nom: string
+  roles: ReadonlyArray<string>
+  type: string
+  contactTechnique?: string
+  contactReferent: Readonly<{
+    nom: string
+    prenom: string
+    poste: string
+    mailContact: string
+    denomination: 'Contact référent' | 'Contact politique de la collectivité'
+  }>
+  telephone: string
+  typologieMembre: string
+  feuillesDeRoute: ReadonlyArray<Readonly<{
+    nom: string
+    montantSubventionAccorde: number
+    montantSubventionFormationAccorde: number
+  }>>
+  totalMontantSubventionFormationAccorde?: number
+  totalMontantSubventionAccorde?: number
+  links: Readonly<{
+    plusDetails?: string
+  }>
+}>
+
+type Query = Readonly<{
+  codeDepartement: string
+}>
+
+function toMembreDetailAvecTotauxReadModel(membre: MembreDetailReadModel): MembreDetailReadModel {
+  const categorieDuMembre = typologieMembre[membre.typologieMembre] ?? typologieMembre.Autre
+  return {
+    ...membre,
+    ...categorieDuMembre === 'autre' && membre.feuillesDeRoute.reduce((result, feuilleDeRoute) => ({
+      totalMontantSubventionAccorde: result.totalMontantSubventionAccorde + feuilleDeRoute.montantSubventionAccorde,
+      totalMontantSubventionFormationAccorde: result.totalMontantSubventionFormationAccorde +
+        feuilleDeRoute.montantSubventionFormationAccorde,
+    }), {
+      totalMontantSubventionAccorde: 0,
+      totalMontantSubventionFormationAccorde: 0,
+    }),
+  }
+}
+
+function toMembreDetailIntitulerReadModel(membre: MembreDetailReadModel): MembreDetailReadModel {
+  const categorieDuMembre = typologieMembre[membre.typologieMembre] ?? typologieMembre.Autre
+  return {
+    ...membre,
+    contactReferent: { ...membre.contactReferent, denomination: categorieDuMembre === 'autre' ? 'Contact référent' : 'Contact politique de la collectivité' },
+    links: { ...categorieDuMembre === 'autre' && { plusDetails: '/' } },
+  }
+}
+
+const typologieMembre: Record<string, string> = {
+  Autre: 'autre',
+  'Préfecture départementale': 'prefecture',
+}
