6257ac1a00e52ce132f67d8a353f1eacb99d6409
Fabien Mercier <fabien.mercier@octo.com>
2025-01-23 14:25:06 +0100

    Supprimer un comité server action

---
+++ src/app/api/actions/supprimerUnComiteAction.test.ts
@@ -0,0 +1,42 @@
+import * as nextCache from 'next/cache'
+
+import { supprimerUnComiteAction } from './supprimerUnComiteAction'
+import * as ssoGateway from '@/gateways/NextAuthAuthentificationGateway'
+import { SupprimerUnComite } from '@/use-cases/commands/SupprimerUnComite'
+
+describe('supprimer un comité action', () => {
+  it('quand un comité est supprimé, alors cela renvoie un succès et le cache de la page appelante est purgé', async () => {
+    // GIVEN
+    vi.spyOn(ssoGateway, 'getSessionSub').mockResolvedValueOnce('userFooId')
+    vi.spyOn(nextCache, 'revalidatePath').mockImplementationOnce(vi.fn())
+    vi.spyOn(SupprimerUnComite.prototype, 'execute').mockResolvedValueOnce('OK')
+
+    // WHEN
+    const messages = await supprimerUnComiteAction({
+      path: '/gouvernance/11',
+      uid: '1',
+      uidGouvernance: 'gouvernanceFooId',
+    })
+
+    // THEN
+    expect(SupprimerUnComite.prototype.execute).toHaveBeenCalledWith({
+      uid: '1',
+      uidGouvernance: 'gouvernanceFooId',
+      uidUtilisateurCourant: 'userFooId',
+    })
+    expect(nextCache.revalidatePath).toHaveBeenCalledWith('/gouvernance/11')
+    expect(messages).toStrictEqual(['OK'])
+  })
+
+  it('quand un comité est supprimé avec un chemin de page non renseigné, alors cela renvoie une erreur', async () => {
+    // WHEN
+    const messages = await supprimerUnComiteAction({
+      path: '',
+      uid: '1',
+      uidGouvernance: 'gouvernanceFooId',
+    })
+
+    // THEN
+    expect(messages).toStrictEqual(['Le chemin doit être renseigné'])
+  })
+})
