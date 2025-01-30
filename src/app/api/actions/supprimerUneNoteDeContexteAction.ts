bf04018aa6db5af02788788f7e9ac8700bfec3e0
dienamo <dienamoustapha@gmail.com>
2025-01-30 15:42:34 +0100

    supprimer note de contexte

---
+++ src/app/api/actions/supprimerUneNoteDeContexteAction.ts
@@ -0,0 +1,41 @@
+'use server'
+
+import { revalidatePath } from 'next/cache'
+import { z } from 'zod'
+
+import prisma from '../../../../prisma/prismaClient'
+import { getSessionSub } from '@/gateways/NextAuthAuthentificationGateway'
+import { PrismaGouvernanceRepository } from '@/gateways/PrismaGouvernanceRepository'
+import { PrismaUtilisateurRepository } from '@/gateways/PrismaUtilisateurRepository'
+import { ResultAsync } from '@/use-cases/CommandHandler'
+import { SupprimerUneNoteDeContexte } from '@/use-cases/commands/SupprimerUneNoteDeContexte'
+
+export async function supprimerUneNoteDeContexteAction(
+  actionParam: ActionParams
+): ResultAsync<ReadonlyArray<string>> {
+  const validationResult = validator.safeParse(actionParam)
+
+  if (validationResult.error) {
+    return validationResult.error.issues.map(({ message }) => message)
+  }
+
+  const supprimerUneNoteDeContexte = new SupprimerUneNoteDeContexte(
+    new PrismaGouvernanceRepository(prisma.gouvernanceRecord, prisma.noteDeContexteRecord),
+    new PrismaUtilisateurRepository(prisma.utilisateurRecord)
+  )
+  const result = await supprimerUneNoteDeContexte.execute({
+    uidEditeur: await getSessionSub(),
+    uidGouvernance: actionParam.uidGouvernance,
+  })
+  revalidatePath(validationResult.data.path)
+  return [result]
+}
+
+type ActionParams = Readonly<{
+  uidGouvernance: string
+  path: string
+}>
+
+const validator = z.object({
+  path: z.string().min(1, { message: 'Le chemin doit être renseigné' }),
+})
