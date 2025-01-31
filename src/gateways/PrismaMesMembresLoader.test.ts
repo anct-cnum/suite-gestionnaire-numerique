a3d11fe85808ef9ee434236ecef5fa419b1a9ceb
Ornella <68587983+Ornella452@users.noreply.github.com>
2025-01-31 16:49:37 +0100

    Gateways : Visualiser la liste des membres de ma gouvernance  (#340)
    * gateways recuperer les membres
    
    * retour commentaire
    
    * refacto en mettant les creates pour les tests dans le testHelper
    
    * Update src/gateways/PrismaMesMembresLoader.ts
    
    Co-authored-by: Fabien <fabien.mercier@octo.com>
    
    * ajout test pour le ORDER BY
    
    * remove test inutile
    
    ---------
    
    Co-authored-by: Fabien <fabien.mercier@octo.com>

---
+++ src/gateways/PrismaMesMembresLoader.test.ts
@@ -0,0 +1,101 @@
+import { PrismaMesMembresLoader } from './PrismaMesMembresLoader'
+import { ajouterUnDepartement, ajouterUneGouvernance, ajouterUneRegion, ajouterUnMembreCommune, ajouterUnMembreDepartement, ajouterUnMembreEpci, ajouterUnMembreSgar, ajouterUnMembreStructure } from './testHelper'
+import prisma from '../../prisma/prismaClient'
+
+describe('mes membres loader', () => {
+  beforeEach(async () => prisma.$queryRaw`START TRANSACTION`)
+
+  afterEach(async () => prisma.$queryRaw`ROLLBACK TRANSACTION`)
+
+  it('quand les membres rattachés à une gouvernance existante sont demandés, alors elle sont renvoyée dans l’ordre alphabétique', async () => {
+    // GIVEN
+    await ajouterUneRegion()
+    await ajouterUnDepartement()
+    await ajouterUneGouvernance()
+    await ajouterUnMembreCommune()
+    await ajouterUnMembreEpci()
+    await ajouterUnMembreStructure()
+    await ajouterUnMembreStructure({ structure: 'Département du Rhône' })
+    await ajouterUnMembreStructure({ role: 'cofinanceur', structure: 'Département du Rhône' })
+    await ajouterUnMembreDepartement()
+    await ajouterUnMembreSgar()
+
+    // WHEN
+    const mesMembresLoader = new PrismaMesMembresLoader(prisma)
+    const mesMembresReadModel = await mesMembresLoader.findMesMembres('69', (mesMembres) => mesMembres)
+
+    // THEN
+    expect(mesMembresReadModel).toStrictEqual({
+      autorisations: {
+        accesMembreValide: false,
+        ajouterUnMembre: false,
+        supprimerUnMembre: false,
+      },
+      departement: 'Rhône',
+      membres: [
+        {
+          contactReferent: {
+            nom: 'Dupont',
+            prenom: 'Justine',
+          },
+          nom: 'Auvergne-Rhône-Alpes',
+          roles: ['recipiendaire'],
+          suppressionDuMembreAutorise: false,
+          typologie: 'Collectivité',
+        },
+        {
+          contactReferent: {
+            nom: 'Didier',
+            prenom: 'Durant',
+          },
+          nom: 'Département du Rhône',
+          roles: ['coporteur', 'cofinanceur'],
+          suppressionDuMembreAutorise: false,
+          typologie: 'Collectivité',
+        },
+        {
+          contactReferent: {
+            nom: 'Dupont',
+            prenom: 'Valérie',
+          },
+          nom: 'Mornant',
+          roles: ['cofinanceur'],
+          suppressionDuMembreAutorise: false,
+          typologie: 'Collectivité',
+        },
+        {
+          contactReferent: {
+            nom: 'Dupont',
+            prenom: 'Valérie',
+          },
+          nom: 'Métropole de Lyon',
+          roles: ['recipiendaire'],
+          suppressionDuMembreAutorise: false,
+          typologie: 'Collectivité, EPCI',
+        },
+        {
+          contactReferent: {
+            nom: 'Henrich',
+            prenom: 'Laetitia',
+          },
+          nom: 'Préfecture du Rhône',
+          roles: ['coporteur'],
+          suppressionDuMembreAutorise: false,
+          typologie: 'Préfecture départementale',
+        },
+        {
+          contactReferent: {
+            nom: 'Dupont',
+            prenom: 'Paul',
+          },
+          nom: 'Rhône',
+          roles: ['cofinanceur'],
+          suppressionDuMembreAutorise: false,
+          typologie: 'Collectivité',
+        },
+      ],
+      roles: [],
+      typologies: [],
+    })
+  })
+})
