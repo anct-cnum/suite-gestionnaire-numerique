91258ac6329f0c97a8c640d033640fb837cc80a8
Fabien Mercier <fabien.mercier@octo.com>
2025-03-04 11:32:12 +0100

    Teasing sur la page des aidants et médiateurs

--- src/app/(connecte)/(avec-menu)/(default)/aidants-et-mediateurs/page.tsx
+++ src/app/(connecte)/(avec-menu)/(default)/aidants-et-mediateurs/page.tsx
@@ -1,8 +1,11 @@
 import { Metadata } from 'next'
+import Image from 'next/image'
 import { ReactElement } from 'react'
 
+import teaser from './teaser.png'
+import Badge from '@/components/shared/Badge/Badge'
+import Icon from '@/components/shared/Icon/Icon'
 import PageTitle from '@/components/shared/PageTitle/PageTitle'
-import TitleIcon from '@/components/shared/TitleIcon/TitleIcon'
 
 export const metadata: Metadata = {
   title: 'Aidants et médiateurs',
@@ -10,9 +13,21 @@ export const metadata: Metadata = {
 
 export default function AidantsEtMediateursController(): ReactElement {
   return (
-    <PageTitle>
-      <TitleIcon icon="group-line" />
-      Aidants et médiateurs
-    </PageTitle>
+    <div className="center fr-mt-6w">
+      <Badge color="green-tilleul-verveine">
+        <Icon
+          classname="fr-text--lead"
+          icon="flashlight-fill"
+        />
+        à venir
+      </Badge>
+      <PageTitle>
+        Identifiez et suivez les aidants et médiateurs de l’inclusion numérique sur votre territoire
+      </PageTitle>
+      <Image
+        alt=""
+        src={teaser}
+      />
+    </div>
   )
 }
