d432995bfe738d52e2411f93cfbfe412185723ce
Fabien Mercier <fabien.mercier@octo.com>
2025-03-04 11:27:50 +0100

    Teasing sur la page des bénéficiaires

--- src/app/(connecte)/(avec-menu)/gouvernance/[codeDepartement]/(avec-menu)/beneficiaires/page.tsx
+++ src/app/(connecte)/(avec-menu)/gouvernance/[codeDepartement]/(avec-menu)/beneficiaires/page.tsx
@@ -1,8 +1,11 @@
 import { Metadata } from 'next'
+import Image from 'next/image'
 import { ReactElement } from 'react'
 
+import teaser from './teaser.png'
+import Badge from '@/components/shared/Badge/Badge'
+import Icon from '@/components/shared/Icon/Icon'
 import PageTitle from '@/components/shared/PageTitle/PageTitle'
-import TitleIcon from '@/components/shared/TitleIcon/TitleIcon'
 
 export const metadata: Metadata = {
   title: 'Bénéficiaires',
@@ -10,9 +13,21 @@ export const metadata: Metadata = {
 
 export default function BeneficiairesController(): ReactElement {
   return (
-    <PageTitle>
-      <TitleIcon icon="community-line" />
-      Bénéficiaires
-    </PageTitle>
+    <div className="center fr-mt-6w">
+      <Badge color="green-tilleul-verveine">
+        <Icon
+          classname="fr-text--lead"
+          icon="flashlight-fill"
+        />
+        à venir
+      </Badge>
+      <PageTitle>
+        Suivez tous les bénéficiaires de financements sur votre territoire et le statut de leur convention
+      </PageTitle>
+      <Image
+        alt=""
+        src={teaser}
+      />
+    </div>
   )
 }
