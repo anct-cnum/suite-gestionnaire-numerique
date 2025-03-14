5b9d175f500a97c0fc8ed80422383e33a01fa334
dienamo <46058257+dienamo@users.noreply.github.com>
2025-03-14 06:46:50 +0000

    reorganisation du context gouvernance

--- src/app/(connecte)/(avec-menu)/gouvernance/[codeDepartement]/(sans-menu)/feuille-de-route/[uidFeuilleDeRoute]/action/[uidAction]/modifier/page.tsx
+++ src/app/(connecte)/(avec-menu)/gouvernance/[codeDepartement]/(sans-menu)/feuille-de-route/[uidFeuilleDeRoute]/action/[uidAction]/modifier/page.tsx
@@ -1,36 +1,14 @@
-import { notFound, redirect } from 'next/navigation'
+import { notFound } from 'next/navigation'
 import { ReactElement } from 'react'
 
 import MenuLateral from '@/components/Action/MenuLateral'
 import ModifierUneAction from '@/components/Action/ModifierUneAction'
 import Notice from '@/components/shared/Notice/Notice'
-import { getSession } from '@/gateways/NextAuthAuthentificationGateway'
-import { PrismaGouvernanceLoader } from '@/gateways/PrismaGouvernanceLoader'
-import { PrismaMembreRepository } from '@/gateways/PrismaMembreRepository'
-import { PrismaUtilisateurLoader } from '@/gateways/PrismaUtilisateurLoader'
 import { actionPresenter } from '@/presenters/actionPresenter'
-import { gouvernancePresenter } from '@/presenters/gouvernancePresenter'
-import { RecupererUneGouvernance } from '@/use-cases/queries/RecupererUneGouvernance'
 
 export default async function ActionModifierController({ params }: Props): Promise<ReactElement> {
   try {
-    const session = await getSession()
-
-    if (!session) {
-      redirect('/connexion')
-    }
     const codeDepartement = (await params).codeDepartement
-    const utilisateurLoader = new PrismaUtilisateurLoader()
-    const utilisateur = await utilisateurLoader.findByUid(session.user.sub)
-    const gouvernanceReadModel = await new RecupererUneGouvernance(
-      new PrismaGouvernanceLoader(),
-      new PrismaMembreRepository()
-    ).handle({
-      codeDepartement,
-      uidUtilisateurCourant: utilisateur.uid,
-    })
-
-    const gouvernanceViewModel = gouvernancePresenter(gouvernanceReadModel, new Date())
     const actionViewModel = actionPresenter(codeDepartement)
 
     return (
@@ -42,7 +20,6 @@ export default async function ActionModifierController({ params }: Props): Promi
           <Notice />
           <ModifierUneAction
             action={actionViewModel}
-            coporteurs={gouvernanceViewModel.sectionMembres.coporteurs}
           />
         </div>
       </div>
