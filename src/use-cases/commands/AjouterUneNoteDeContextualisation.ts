29d800c7fcb94160100706532cb2d2fbf797e2c6
Philippe <philippe.martinez@ext.anct.gouv.fr>
2025-04-16 18:07:58 +0200

    530 ajout de la note de contextualisation - Ajout de ifchier (#537)

---
+++ src/use-cases/commands/AjouterUneNoteDeContextualisation.ts
@@ -0,0 +1,53 @@
+import { CommandHandler } from '../CommandHandler'
+import { GetFeuilleDeRouteRepository, UpdateFeuilleDeRouteRepository } from './shared/FeuilleDeRouteRepository'
+import { GetUtilisateurRepository } from './shared/UtilisateurRepository'
+import { FeuilleDeRouteUid, NoteDeContextualisation } from '@/domain/FeuilleDeRoute'
+import { UtilisateurUid } from '@/domain/Utilisateur'
+import { isOk } from '@/shared/lang'
+
+export class AjouterUneNoteDeContextualisation implements CommandHandler<Command> {
+  readonly #date: Date
+  readonly #feuilleDeRouteRepository: FeuilleDeRouteRepository
+  readonly #utilisateurRepository: UtilisateurRepository
+
+  constructor(
+    feuilleDeRouteRepository: FeuilleDeRouteRepository,
+    utilisateurRepository: UtilisateurRepository,
+    date: Date
+  ) {
+    this.#feuilleDeRouteRepository = feuilleDeRouteRepository
+    this.#utilisateurRepository = utilisateurRepository
+    this.#date = date
+  }
+
+  async handle(command: Command): Promise<'OK' | Failure> {
+    const editeur = await this.#utilisateurRepository.get(command.uidEditeur)
+    const feuilleDeRoute = await this.#feuilleDeRouteRepository.get(new FeuilleDeRouteUid(command.uidFeuilleDeRoute))
+    if (!feuilleDeRoute.peutEtreGereePar(editeur)) {
+      return 'utilisateurNePeutPasAjouterNoteDeContextualisation'
+    }
+  
+    const noteDeContextualisation = new NoteDeContextualisation(
+      this.#date,
+      new UtilisateurUid(editeur.state.uid),
+      command.contenu
+    )
+    const result = feuilleDeRoute.ajouterUneNoteDeContextualisation(noteDeContextualisation)
+    if (!isOk(result)) {
+      return result as Failure
+    }
+    await this.#feuilleDeRouteRepository.update(feuilleDeRoute)
+    return 'OK'
+  }
+}
+
+type Command = Readonly<{
+  contenu: string
+  uidEditeur: string
+  uidFeuilleDeRoute: string
+}>
+
+type Failure = 'noteDeContextualisationDejaExistante' | 'utilisateurNePeutPasAjouterNoteDeContextualisation'
+type FeuilleDeRouteRepository = GetFeuilleDeRouteRepository & UpdateFeuilleDeRouteRepository
+
+type UtilisateurRepository = GetUtilisateurRepository 
