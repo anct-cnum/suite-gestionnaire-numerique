a7ae4e2a2358dd4a3a39ad2c31289452f60a4870
Adrien T <adrien.turpin-ext@ecomesure.com>
2025-04-25 15:02:08 +0200

    Fix visual gap + tout effacer

--- src/components/shared/Badge/Badge.tsx
+++ src/components/shared/Badge/Badge.tsx
@@ -2,7 +2,7 @@ import { PropsWithChildren, ReactElement } from 'react'
 
 export default function Badge({ children, color, icon = false }: Props): ReactElement {
   return (
-    <p className={`fr-badge fr-badge--${color} ${icon ? '' : 'fr-badge--no-icon'}`}>
+    <p className={`fr-badge fr-badge--${color} ${icon ? '' : 'fr-badge--no-icon'} fr-m-1v`}>
       {children}
     </p>
   )
