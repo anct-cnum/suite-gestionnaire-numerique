8439a15c20daeb613ea5ab2e033d2e0d389fb637
Philippe <philippe.martinez@ext.anct.gouv.fr>
2025-05-26 11:58:46 +0200

    593: suppression du lien en dur vers la gouvernance 11. (#612)

--- src/app/(connecte)/(avec-menu)/(default)/tableau-de-bord/page.tsx
+++ src/app/(connecte)/(avec-menu)/(default)/tableau-de-bord/page.tsx
@@ -1,16 +1,28 @@
 import { Metadata } from 'next'
+import { redirect } from 'next/navigation'
 import { ReactElement } from 'react'
 
 import Notice from '@/components/shared/Notice/Notice'
 import TableauDeBord from '@/components/TableauDeBord/TableauDeBord'
+import { getSession } from '@/gateways/NextAuthAuthentificationGateway'
+import { PrismaUtilisateurLoader } from '@/gateways/PrismaUtilisateurLoader'
 import { tableauDeBordPresenter } from '@/presenters/tableauDeBordPresenter'
 
 export const metadata: Metadata = {
   title: 'Mon tableau de bord',
 }
 
-export default function TableauDeBordController(): ReactElement {
-  const tableauDeBordViewModel = tableauDeBordPresenter()
+export default async function TableauDeBordController(): Promise<ReactElement> {
+  const session = await getSession()
+
+  if (!session) {
+    redirect('/connexion')
+  }
+
+  const utilisateurLoader = new PrismaUtilisateurLoader()
+  const utilisateur = await utilisateurLoader.findByUid(session.user.sub)
+  
+  const tableauDeBordViewModel = tableauDeBordPresenter(utilisateur.departementCode ?? '')
 
   return (
     <>
