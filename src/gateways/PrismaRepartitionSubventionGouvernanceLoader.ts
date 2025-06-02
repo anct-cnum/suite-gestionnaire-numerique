89aa3cc680c69c36d31dc0cd79cf57160a5c221d
Philippe Martinez <philippe.martinez@ext.anct.gouv.fr>
2025-06-02 11:58:41 +0200

    Prise en compte des demandes de subvention accordee dans le calcul du restant sur les envloppes ventiles

---
+++ src/gateways/PrismaRepartitionSubventionGouvernanceLoader.ts
@@ -0,0 +1,35 @@
+import prisma from '../../prisma/prismaClient'
+import { RepartitionSubventionGouvernanceLoader } from '@/use-cases/queries/RecupererRepartitionSubventionGouvernance'
+import { StatutSubvention } from '@/use-cases/queries/shared/ActionReadModel'
+
+export class PrismaRepartitionSubventionGouvernanceLoader implements RepartitionSubventionGouvernanceLoader {
+  // eslint-disable-next-line @typescript-eslint/class-methods-use-this
+  async get(uidGouvernance: string): Promise<ReadonlyMap<string, number>> {
+    const demandesDeSubventionsAccordees = await prisma.demandeDeSubventionRecord.findMany({
+      include: {
+        enveloppe: true,
+      },
+      where: {
+        action: {
+          feuilleDeRoute: {
+            gouvernanceDepartementCode: uidGouvernance,
+          },
+        },
+        statut: StatutSubvention.ACCEPTEE,
+      },
+    })
+
+    // cumul des subventions acceptées par enveloppe
+    const repartitionParEnveloppe = 
+            demandesDeSubventionsAccordees.reduce<Record<string, number>>((acc, demandeDeSubvention) => {
+              const enveloppeId = String(demandeDeSubvention.enveloppeFinancementId)
+              const montantActuel = acc[enveloppeId] ?? 0
+              return {
+                ...acc,
+                [enveloppeId]: montantActuel + demandeDeSubvention.subventionDemandee,
+              }
+            }, {})
+
+    return new Map(Object.entries(repartitionParEnveloppe))
+  }
+}
