4b3b0ba52a1425629ba65c58508b33073eefdb75
Philippe Martinez <philippe.martinez@ext.anct.gouv.fr>
2025-06-25 10:16:48 +0200

    Fix role dans la migration prisma (integration dataspace)

--- prisma/migrations/20250619151605_dataspace_integration/migration.sql
+++ prisma/migrations/20250619151605_dataspace_integration/migration.sql
@@ -11,6 +11,50 @@ BEGIN
    END IF;
 END
 $do$;
+
+
+DO
+$do$
+BEGIN
+   IF EXISTS (
+      SELECT FROM pg_catalog.pg_roles
+      WHERE  rolname = 'app_python') THEN
+
+      RAISE NOTICE 'Role "app_python" already exists. Skipping.';
+   ELSE
+      CREATE ROLE app_python LOGIN PASSWORD 'my_password';
+   END IF;
+END
+$do$;
+
+DO
+$do$
+BEGIN
+   IF EXISTS (
+      SELECT FROM pg_catalog.pg_roles
+      WHERE  rolname = 'min_dev') THEN
+
+      RAISE NOTICE 'Role "min_dev" already exists. Skipping.';
+   ELSE
+      CREATE ROLE min_dev  LOGIN PASSWORD 'my_password';
+   END IF;
+END
+$do$;
+
+DO
+$do$
+BEGIN
+   IF EXISTS (
+      SELECT FROM pg_catalog.pg_roles
+      WHERE  rolname = 'min_scalingo') THEN
+
+      RAISE NOTICE 'Role "min_scalingo" already exists. Skipping.';
+   ELSE
+      CREATE ROLE min_scalingo  LOGIN PASSWORD 'my_password';
+   END IF;
+END
+$do$;
+
 --
 -- Name: admin; Type: SCHEMA; Schema: -; Owner: sonum
 --
