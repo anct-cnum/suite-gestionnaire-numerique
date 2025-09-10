152bf4c60d88027b4a16fb311c736665a9f6059c
Philippe Martinez <philippe.martinez@ext.anct.gouv.fr>
2025-09-10 18:19:26 +0200

    806: historique en base => suppression de l'historique des accompagnements par département

--- src/gateways/tableauDeBord/PrismaAccompagnementsRealisesLoader.ts
+++ src/gateways/tableauDeBord/PrismaAccompagnementsRealisesLoader.ts
@@ -1,113 +1,7 @@
-/* eslint-disable @stylistic/quote-props */
-
 import prisma from '../../../prisma/prismaClient'
 import { reportLoaderError } from '../shared/sentryErrorReporter'
 import { AccompagnementsRealisesLoader, AccompagnementsRealisesReadModel } from '@/use-cases/queries/RecupererAccompagnementsRealises'
 import { ErrorReadModel } from '@/use-cases/queries/shared/ErrorReadModel'
-
-const accompagnementsPrecedents = {
-  '01' : 73203,
-  '02' : 55494,
-  '03' : 25344,
-  '04' : 16797,
-  '05' : 25265,
-  '06' : 52805,
-  '07' : 30028,
-  '08' : 50136,
-  '09' : 15088,
-  '10' : 40838,
-  '11' : 25567,
-  '12' : 29373,
-  '13' : 99259,
-  '14' : 56448,
-  '15' : 22662,
-  '16' : 34858,
-  '17' : 49176,
-  '18':	29745,
-  '19':	24835,
-  '21':	41485,
-  '22':	52508,
-  '23':	12628,
-  '24':	41978,
-  '25':	40156,
-  '26':	39977,
-  '27':	43201,
-  '28':	17034,
-  '29':	57078,
-  '2A':	6222,
-  '2B':	12951,
-  '30':	60576,
-  '31':	64293,
-  '32':	16482,
-  '33':	120061,
-  '34':	103205,
-  '35':	66024,
-  '36':	33153,
-  '37':	32018,
-  '38':	37971,
-  '39':	18457,
-  '40':	37778,
-  '41':	28875,
-  '42':	44376,
-  '43':	28105,
-  '44':	59232,
-  '45':	32924,
-  '46':	25118,
-  '47':	53169,
-  '48':	24098,
-  '49':	45939,
-  '50':	40917,
-  '51':	37904,
-  '52':	27999,
-  '53':	21277,
-  '54':	60632,
-  '55':	30011,
-  '56':	69487,
-  '57':	61547,
-  '58':	36422,
-  '59':	164189,
-  '60':	20814,
-  '61':	32516,
-  '62':	71215,
-  '63':	33460,
-  '64':	48566,
-  '65':	25975,
-  '66':	67940,
-  '67':	52144,
-  '68':	32337,
-  '69':	97769,
-  '70':	54105,
-  '71':	42693,
-  '72':	59361,
-  '73':	26467,
-  '74':	33269,
-  '75':	101437,
-  '76':	63568,
-  '77':	41533,
-  '78':	38215,
-  '79':	28293,
-  '80':	27417,
-  '81':	41483,
-  '82':	35388,
-  '83':	66804,
-  '84':	55917,
-  '85':	52031,
-  '86':	41101,
-  '87':	41051,
-  '88':	45978,
-  '89':	24331,
-  '90':	15640,
-  '91':	47277,
-  '92':	41316,
-  '93':	90999,
-  '94':	55436,
-  '95':	36386,
-  '971':	40934,
-  '972':	45001,
-  '973':	17797,
-  '974':	87751,
-  '976':	25783,
-}
 
 export class PrismaAccompagnementsRealisesLoader implements AccompagnementsRealisesLoader {
   async get(territoire: string): Promise<AccompagnementsRealisesReadModel | ErrorReadModel> {
@@ -156,15 +50,7 @@ export class PrismaAccompagnementsRealisesLoader implements AccompagnementsReali
         `
       }
 
-      let nombreTotal = Number(totalResult[0]?.total_accompagnements ?? 0)
-
-      // Ajout des accompagnements précédents
-      if (territoire === 'France') {
-        // Pour la France, on somme tous les accompagnements précédents
-        nombreTotal += Object.values(accompagnementsPrecedents).reduce((sum, value) => sum + value, 0)
-      } else if (territoire in accompagnementsPrecedents) {
-        nombreTotal += accompagnementsPrecedents[territoire as keyof typeof accompagnementsPrecedents]
-      }
+      const nombreTotal = Number(totalResult[0]?.total_accompagnements ?? 0)
 
       // Récupération de la répartition mensuelle (activités COOP uniquement car on a les dates)
       let repartitionResult: Array<{ mois: string; nombre: bigint }>
