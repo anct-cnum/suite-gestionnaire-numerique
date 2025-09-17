d1a4fa3b4a2cc86650fa82c6c0f4e8c806526c6c
Philippe Martinez <philippe.martinez@ext.anct.gouv.fr>
2025-09-17 15:26:17 +0200

    675 : MAJ de la config matomo : branchement sur le matomo de l'incubateur ANCT

--- public/matomo-v1.js
+++ public/matomo-v1.js
@@ -2,9 +2,9 @@ var _paq = window._paq = window._paq || [];
 _paq.push(['trackPageView']);
 _paq.push(['enableLinkTracking']);
 (function() {
-  var u="https://stats.beta.gouv.fr/";
+  var u="https://matomo.incubateur.anct.gouv.fr/";
   _paq.push(['setTrackerUrl', u+'matomo.php']);
-  _paq.push(['setSiteId', '200']);
+  _paq.push(['setSiteId', '27']);
   var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
   g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
 })();
