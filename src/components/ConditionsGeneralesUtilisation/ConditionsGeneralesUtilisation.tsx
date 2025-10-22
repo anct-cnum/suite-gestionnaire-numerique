115a96545fb2720e2e8557c657211281ee6ee5d3
techlag <adrien.turpin@ext.anct.gouv.fr>
2025-10-22 13:42:21 +0200

    Ajout de la page des conditions generales d utilisation

---
+++ src/components/ConditionsGeneralesUtilisation/ConditionsGeneralesUtilisation.tsx
@@ -0,0 +1,261 @@
+import { ReactElement } from 'react'
+
+import PageTitle from '../shared/PageTitle/PageTitle'
+
+export default function ConditionsGeneralesUtilisation(): ReactElement {
+  return (
+    <div className="fr-grid-row fr-grid-row--center">
+      <article
+        aria-labelledby="cgu-title"
+        className="fr-col-12 fr-col-lg-10"
+      >
+        <PageTitle>
+          Conditions générales d&apos;utilisation
+        </PageTitle>
+
+        <p>
+          Les présentes conditions générales d&apos;utilisation (ci-après &quot;CGU&quot;) fixent le cadre juridique de
+          Mon inclusion numérique (ci-après la &quot;Plateforme&quot;) et définissent les conditions d&apos;accès et
+          d&apos;utilisation proposées à l&apos;Utilisateur.
+        </p>
+
+        <section
+          aria-labelledby="champ-application-title"
+          className="fr-mb-4w"
+        >
+          <h2
+            className="fr-h6"
+            id="champ-application-title"
+          >
+            Article 1 - Champ d&apos;application
+          </h2>
+          <p>
+            Le présent document a pour objet d&apos;encadrer l&apos;utilisation de la Plateforme qui est d&apos;accès
+            libre et gratuit à l&apos;Utilisateur.
+          </p>
+          <p>
+            Toute utilisation de la Plateforme est subordonnée au respect intégral des présentes CGU.
+          </p>
+        </section>
+
+        <section
+          aria-labelledby="definitions-title"
+          className="fr-mb-4w"
+        >
+          <h2
+            className="fr-h6"
+            id="definitions-title"
+          >
+            Article 2 - Définitions
+          </h2>
+          <p>
+            &quot;Éditeur&quot; de la plateforme est l&apos;Agence nationale de la cohésion des territoires (ANCT).
+          </p>
+          <p>
+            &quot;Service&quot; désigne toutes les fonctionnalités offertes par la Plateforme pour répondre à ses
+            finalités.
+          </p>
+          <p>
+            &quot;Utilisateur&quot; désigne tout professionnel de l&apos;inclusion numérique habilité, personne
+            physique, qui dispose d&apos;un compte sur la Plateforme.
+          </p>
+        </section>
+
+        <section
+          aria-labelledby="objet-title"
+          className="fr-mb-4w"
+        >
+          <h2
+            className="fr-h6"
+            id="objet-title"
+          >
+            Article 3 - Objet
+          </h2>
+          <p>
+            La Plateforme est un service public numérique développé par le programme Société Numérique de l&apos;ANCT.
+            Elle a deux objectifs :
+          </p>
+          <ul>
+            <li>
+              Mesurer précisément l&apos;impact de la politique publique d&apos;inclusion numérique, notamment en
+              évaluant la montée en compétence des bénéficiaires d&apos;accompagnement au numérique ;
+            </li>
+            <li>
+              Piloter l&apos;ensemble des dispositifs d&apos;inclusion numérique déployés à une échelle
+              nationale, régionale, départementale, communale.
+            </li>
+          </ul>
+          <p>
+            Pour cela, la Plateforme expose et administre les données de l&apos;entrepôt de données de
+            l&apos;inclusion numérique de l&apos;ANCT ayant vocation à rassembler l&apos;ensemble des données
+            relatives à l&apos;inclusion numérique, permettant à tout professionnel de l&apos;inclusion numérique
+            habilité de :
+          </p>
+          <ul>
+            <li>
+              Évaluer les besoins en termes d&apos;inclusion numérique de son territoire ;
+            </li>
+            <li>
+              Prendre connaissance des dispositifs d&apos;inclusion numérique de son territoire et d&apos;en
+              analyser l&apos;impact ;
+            </li>
+            <li>
+              Construire, déployer, et mesurer l&apos;impact du ou des dispositifs d&apos;inclusion numérique
+              qu&apos;il ou elle pilote.
+            </li>
+          </ul>
+        </section>
+
+        <section
+          aria-labelledby="fonctionnalites-title"
+          className="fr-mb-4w"
+        >
+          <h2
+            className="fr-h6"
+            id="fonctionnalites-title"
+          >
+            Article 4 - Fonctionnalités
+          </h2>
+
+          <h3 className="fr-text--md fr-mt-3w">
+            4.1 Accès et connexion à l&apos;espace Utilisateur
+          </h3>
+          <p>
+            L&apos;Utilisateur s&apos;authentifie et se connecte à son espace via ProConnect. Il renseigne son adresse
+            courriel professionnelle et son mot de passe.
+          </p>
+
+          <h3 className="fr-text--md fr-mt-3w">
+            4.2 Fonctionnalités ouvertes à l&apos;Utilisateur
+          </h3>
+          <p>
+            Chaque Utilisateur peut notamment :
+          </p>
+          <ul>
+            <li>
+              Accéder à un tableau de bord qui recense les tâches qu&apos;il doit réaliser (demandes de
+              subvention par exemple) ;
+            </li>
+            <li>
+              Bénéficier d&apos;une vision globale sur les gouvernances territoriales et les feuilles de route
+              déposées ;
+            </li>
+            <li>
+              Suivre les conventions et financements en cours ;
+            </li>
+            <li>
+              Visualiser les membres bénéficiaires par financements ;
+            </li>
+            <li>
+              Identifier les médiateurs et aidants numériques et leur niveau de formation ou de
+              certification ;
+            </li>
+            <li>
+              Accéder à une liste de lieux d&apos;inclusion numérique.
+            </li>
+          </ul>
+          <p>
+            Il peut aussi exporter les données par filtres et générer un rapport avec des chiffres clés sur
+            l&apos;inclusion numérique.
+          </p>
+          <p>
+            L&apos;Utilisateur peut également, à tout moment, modifier ses informations personnelles, tester son adresse
+            courriel, télécharger ses données et supprimer son compte. Il peut notamment paramétrer son compte pour
+            recevoir toutes les notifications sur son adresse courriel ou recevoir un récapitulatif hebdomadaire.
+          </p>
+        </section>
+
+        <section
+          aria-labelledby="responsabilites-title"
+          className="fr-mb-4w"
+        >
+          <h2
+            className="fr-h6"
+            id="responsabilites-title"
+          >
+            Article 5 - Responsabilités
+          </h2>
+
+          <h3 className="fr-text--md fr-mt-3w">
+            5.1 L&apos;Éditeur de la Plateforme
+          </h3>
+          <p>
+            Les sources des informations diffusées sur la Plateforme sont réputées fiables mais l&apos;Éditeur
+            ne garantit pas qu&apos;elle soit exempte de défauts, d&apos;erreurs ou d&apos;omissions.
+          </p>
+          <p>
+            L&apos;Éditeur s&apos;engage à la sécurisation de la Plateforme, notamment en prenant toutes les
+            mesures nécessaires permettant de garantir la sécurité et la confidentialité des informations
+            fournies. Il ne peut être tenu responsable en cas d&apos;usurpation d&apos;identité ou de toute
+            utilisation frauduleuse de la Plateforme.
+          </p>
+          <p>
+            L&apos;Éditeur fournit les moyens nécessaires et raisonnables pour assurer un accès continu, sans
+            contrepartie financière, au Service.
+          </p>
+          <p>
+            Il ne peut être tenu responsable des pertes et/ou préjudices, de quelque nature qu&apos;ils soient,
+            qui pourraient être causés à la suite d&apos;un dysfonctionnement ou d&apos;une indisponibilité de la
+            Plateforme. De telles situations n&apos;ouvriront droit à aucune compensation ou indemnité d&apos;aucune
+            nature dont financière.
+          </p>
+          <p>
+            En cas de manquement à une ou plusieurs des stipulations des présentes CGU, l&apos;Éditeur
+            s&apos;autorise à suspendre ou révoquer n&apos;importe quel compte et toutes les actions réalisées
+            par ce biais, s&apos;il estime que l&apos;usage réalisé de la Plateforme porte préjudice à son image ou
+            ne correspond pas aux exigences de sécurité. Et le cas échéant, à informer l&apos;employeur de
+            l&apos;Utilisateur des désordres et / ou comportements observés.
+          </p>
+
+          <h3 className="fr-text--md fr-mt-3w">
+            5.2 L&apos;Utilisateur
+          </h3>
+          <p>
+            L&apos;Utilisateur s&apos;assure de garder son mot de passe secret. Toute divulgation du mot de
+            passe, quelle que soit sa forme, est interdite. Il assume les risques liés à l&apos;utilisation de son
+            identifiant et mot de passe.
+          </p>
+          <p>
+            L&apos;Utilisateur veille à ne pas commercialiser, diffuser publiquement ou à communiquer à des
+            tiers qui n&apos;ont pas à en connaître les informations et données de la Plateforme. Il s&apos;engage
+            à garantir la confidentialité et la sécurité des données, notamment des bénéficiaires.
+          </p>
+          <p>
+            L&apos;Utilisateur s&apos;engage à ne pas mettre en ligne de contenus ou informations contraires aux
+            dispositions légales et réglementaires en vigueur et ne pas renseigner de données non nécessaires
+            aux finalités ou des données sensibles notamment dans les zones de champs libres.
+          </p>
+          <p>
+            Toute information transmise par l&apos;Utilisateur est de sa seule responsabilité. Il est rappelé
+            que toute personne procédant à une fausse déclaration pour elle-même ou pour autrui s&apos;expose,
+            notamment, aux sanctions prévues à l&apos;article 441-1 du code pénal, prévoyant des peines pouvant
+            aller jusqu&apos;à trois ans d&apos;emprisonnement et 45&nbsp;000 euros d&apos;amende.
+          </p>
+          <p>
+            L&apos;Utilisateur a été habilité à prendre connaissance des informations sur la Plateforme et
+            certifie qu&apos;il utilise la Plateforme pour les seules finalités liées à l&apos;inclusion numérique
+            et l&apos;évaluation de la montée en compétence de la population au numérique.
+          </p>
+        </section>
+
+        <section
+          aria-labelledby="mise-a-jour-title"
+          className="fr-mb-4w"
+        >
+          <h2
+            className="fr-h6"
+            id="mise-a-jour-title"
+          >
+            Article 6 - Mise à jour des conditions générales d&apos;utilisation
+          </h2>
+          <p>
+            Les termes des présentes conditions générales d&apos;utilisation peuvent être amendés à tout
+            moment, sans préavis, en fonction des modifications apportées à la Plateforme, de l&apos;évolution
+            de la législation ou pour tout autre motif jugé nécessaire. Chaque modification donne lieu à une
+            nouvelle version qui doit être acceptée par l&apos;Utilisateur.
+          </p>
+        </section>
+      </article>
+    </div>
+  )
+}
