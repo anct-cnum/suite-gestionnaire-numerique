c10c22a3b4b502d4abcde1b8a123da12f5ba1caf
techlag <adrien.turpin@ext.anct.gouv.fr>
2025-11-10 18:05:51 +0100

    correction de la taille des icons

--- src/components/shared/TitleIcon/TitleIcon.tsx
+++ src/components/shared/TitleIcon/TitleIcon.tsx
@@ -4,10 +4,12 @@ import styles from './TitleIcon.module.css'
 
 export default function TitleIcon({ background = 'blue', icon }: Props): ReactElement {
   return (
-    <span
-      aria-hidden="true"
-      className={`fr-icon-${icon} ${styles[background]} ${styles.icon}`}
-    />
+    <div className={`${styles[background]} ${styles.container} fr-mr-2w`}>
+      <span
+        aria-hidden="true"
+        className={`fr-icon-${icon} ${styles.icon}`}
+      />
+    </div>
   )
 }
 
