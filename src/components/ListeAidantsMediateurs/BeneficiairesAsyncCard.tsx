2d38c4fb1c000671e8e47e36ee220244ac312250
techlag <adrien.turpin@ext.anct.gouv.fr>
2025-11-18 18:00:49 +0100

    ajout de l info bulle Bénéficiaires accompagnés

--- src/components/ListeAidantsMediateurs/BeneficiairesAsyncCard.tsx
+++ src/components/ListeAidantsMediateurs/BeneficiairesAsyncCard.tsx
@@ -5,6 +5,7 @@ import { ReactElement, use } from 'react'
 import { parseTextWithBold } from '../../shared/textFormatting'
 import { ErrorViewModel, isErrorViewModel } from '@/components/shared/ErrorViewModel'
 import { formaterEnNombreFrancais } from '@/presenters/shared/number'
+import Information from '@/components/shared/Information/Information'
 
 export default function BeneficiairesAsyncCard({
   hasActiveFilters,
@@ -41,7 +42,7 @@ export default function BeneficiairesAsyncCard({
       </div>
     )
   }
-  
+
   return (
     <div
       className="fr-col-12 fr-col-md-4"
@@ -62,6 +63,14 @@ export default function BeneficiairesAsyncCard({
         </div>
         <div className="fr-text--sm fr-text-title--blue-france fr-text--bold fr-m-0">
           Bénéficiaires accompagnés
+          <Information  label="Sur la Coop, après un accompagnement, le médiateur peut choisir d’enregistrer ou non la personne accompagnée dans sa liste de bénéficiaires et de la rattacher à cet accompagnement.
+
+Exemple sur un mois :
+
+Une personne suivie est accompagnée trois fois. Elle compte pour un seul bénéficiaire.
+Une personne anonyme est accompagnée deux fois. Elle compte pour deux bénéficiaires.
+Le total affiché pour ce mois sera donc 3 bénéficiaires : 1 suivi + 2 anonymes "
+          />
         </div>
         <div className="fr-text--sm fr-text-title--blue-france fr-m-0">
           {parseTextWithBold('sur les 30 derniers jours')}
