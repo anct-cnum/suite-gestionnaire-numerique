cf09043a51c3af734177a251e36919810ed3f087
Philippe Martinez <philippe.martinez@ext.anct.gouv.fr>
2025-11-19 21:54:48 +0100

    Fix => metatadata dans un composant client

--- src/app/vitrine/donnees-territoriales/page.tsx
+++ src/app/vitrine/donnees-territoriales/page.tsx
@@ -1,36 +1,35 @@
-import { Metadata } from 'next'
 import { ReactElement } from 'react'
 
 import DonneesTerritoriales from '@/components/vitrine/DonneesTerritoriales/DonneesTerritoriales'
 
-export const metadata: Metadata = {
-  description: 'Découvrez les données publiques d\'inclusion numérique par territoire : départements, régions et national. Accédez aux statistiques sur les lieux d\'inclusion, médiateurs numériques et accompagnements réalisés en France.',
-  keywords: [
-    'inclusion numérique',
-    'données territoriales',
-    'France',
-    'départements',
-    'régions',
-    'statistiques',
-    'médiateurs numériques',
-    'accompagnement numérique',
-    'Aidants Connect',
-    'Conseiller Numérique',
-    'France Services',
-  ],
-  openGraph: {
-    description: 'Découvrez les données publiques d\'inclusion numérique par territoire en France',
-    locale: 'fr_FR',
-    siteName: 'Mon Inclusion Numérique',
-    title: 'Données territoriales - Inclusion Numérique',
-    type: 'website',
-  },
-  robots: {
-    follow: false,
-    index: false,
-  },
-  title: 'Données territoriales - Inclusion Numérique',
-}
+// export const metadata: Metadata = {
+//   description: 'Découvrez les données publiques d\'inclusion numérique par territoire : départements, régions et national. Accédez aux statistiques sur les lieux d\'inclusion, médiateurs numériques et accompagnements réalisés en France.',
+//   keywords: [
+//     'inclusion numérique',
+//     'données territoriales',
+//     'France',
+//     'départements',
+//     'régions',
+//     'statistiques',
+//     'médiateurs numériques',
+//     'accompagnement numérique',
+//     'Aidants Connect',
+//     'Conseiller Numérique',
+//     'France Services',
+//   ],
+//   openGraph: {
+//     description: 'Découvrez les données publiques d\'inclusion numérique par territoire en France',
+//     locale: 'fr_FR',
+//     siteName: 'Mon Inclusion Numérique',
+//     title: 'Données territoriales - Inclusion Numérique',
+//     type: 'website',
+//   },
+//   robots: {
+//     follow: false,
+//     index: false,
+//   },
+//   title: 'Données territoriales - Inclusion Numérique',
+// }
 
 export default function DonneesTerritorialesController(): ReactElement {
   return <DonneesTerritoriales />
