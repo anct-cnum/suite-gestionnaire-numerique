391a0cb65f7ff346f65e112eca426d5fcc7547cb
techlag <adrien.turpin@ext.anct.gouv.fr>
2025-11-25 10:18:54 +0100

    ajout de la page lieu d inclusion numerique

--- src/app/vitrine/donnees-territoriales/(with-layout)/lieux-inclusion/[niveau]/[[...code]]/page.tsx
+++ src/app/vitrine/donnees-territoriales/(with-layout)/lieux-inclusion/[niveau]/[[...code]]/page.tsx
@@ -1,16 +1,40 @@
-'use client'
-
+import { notFound } from 'next/navigation'
 import { ReactElement } from 'react'
 
-import AlerteConstruction from '@/components/shared/AlerteConstruction/AlerteConstruction'
+import LieuxInclusionVitrine from '@/components/vitrine/LieuxInclusion/LieuxInclusionVitrine'
+import { PrismaLieuxInclusionNumeriqueLoader } from '@/gateways/PrismaLieuxInclusionNumeriqueLoader'
+import { lieuxInclusionNumeriquePresenter } from '@/presenters/lieuxInclusionNumeriquePresenter'
 
-export default function LieuxInclusion(): ReactElement {
-  return (
-    <>
-      <h1 className="fr-h1">
-        Lieux d&apos;inclusion
-      </h1>
-      <AlerteConstruction />
-    </>
-  )
+export default async function LieuxInclusion({ params }: Props): Promise<ReactElement> {
+  const { code, niveau } = await params
+
+  // Validation : niveau doit être 'national' ou 'departement'
+  if (niveau !== 'national' && niveau !== 'departement') {
+    notFound()
+  }
+
+  // Validation : si niveau = 'departement', code est obligatoire
+  if (niveau === 'departement' && (code === undefined || code.length === 0)) {
+    notFound()
+  }
+
+  // Extraction du code département si présent
+  const codeDepartement = niveau === 'departement' && code !== undefined ? code[0] : undefined
+
+  // Charger les données
+  const loader = new PrismaLieuxInclusionNumeriqueLoader()
+  const readModel = codeDepartement === undefined
+    ? await loader.getNational()
+    : await loader.getDepartemental(codeDepartement)
+
+  const viewModel = lieuxInclusionNumeriquePresenter(readModel)
+
+  return <LieuxInclusionVitrine viewModel={viewModel} />
 }
+
+type Props = Readonly<{
+  params: Promise<{
+    code?: ReadonlyArray<string>
+    niveau: string
+  }>
+}>
