fde04a73357ea666a2da5841d77e8842750cb1a0
Philippe Martinez <philippe.martinez@ext.anct.gouv.fr>
2025-11-28 12:54:52 +0100

    Build erreur again : le soucis reste malgré le revert du DSFR=> fix temporaire en augmentant la stack

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-ssh -N -f -L $DS_BDD_IP $DS_BASTION_IP -p $DS_BASTION_PORT && yarn prisma:generate  && next build
+ssh -N -f -L $DS_BDD_IP $DS_BASTION_IP -p $DS_BASTION_PORT && yarn prisma:generate  && node --stack-size=8192 ./node_modules/.bin/next build
