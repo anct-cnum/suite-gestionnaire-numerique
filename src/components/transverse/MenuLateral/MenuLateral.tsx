f245091b9d9b76af79109306fc1bb7a41fecd825
techlag <adrien.turpin@ext.anct.gouv.fr>
2026-01-06 09:55:11 +0100

    retrait menu tableau de board suivi des post CoNum pour les non Admin

--- src/components/transverse/MenuLateral/MenuLateral.tsx
+++ src/components/transverse/MenuLateral/MenuLateral.tsx
@@ -52,12 +52,7 @@ export default function MenuLateral({ children }: Readonly<PropsWithChildren>): 
       icon: 'map-pin-2-line',
       label: 'Lieux d\'inclusion',
       url: '/lieux-inclusion',
-    },
-    {
-      customIcon: `${process.env.NEXT_PUBLIC_HOST}/conum-full.svg`,
-      label: 'Suivi des postes CoNum',
-      url: '/postes-conseiller-numerique',
-    },
+    }
   ]
 
   const menusAVenir = [
