25a7e74f2020b0788cf5b2f824673e6fa4a6d8ce
Kevin GALLET <kevingallet@gmail.com>
2026-04-16 15:14:07 +0200

    fix: sidemenu responsiveness

--- src/app/(connecte)/(avec-menu)/(default)/layout.tsx
+++ src/app/(connecte)/(avec-menu)/(default)/layout.tsx
@@ -20,12 +20,12 @@ export default async function Layout({ children }: Readonly<PropsWithChildren>):
 
   return (
     <div className="fr-grid-row">
-      <div className="fr-col-3">
+      <div className="fr-col-12 fr-col-md-3 fr-col-xl-2">
         <MenuActifProvider>
           <MenuLateral contexte={contexte} />
         </MenuActifProvider>
       </div>
-      <div className="fr-col-9 fr-pl-7w menu-border">{children}</div>
+      <div className="fr-col-12 fr-col-md-9 fr-col-xl-10 fr-pl-md-7w menu-border">{children}</div>
     </div>
   )
 }
