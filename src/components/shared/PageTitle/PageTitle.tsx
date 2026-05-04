d7c55a4a1f659da33a64989de4251219ab257dfc
techlag <adrien.turpin@ext.anct.gouv.fr>
2026-05-04 10:42:24 +0200

    fix(PageTitle): corriger la taille du texte à 36px/40px selon le design

--- src/components/shared/PageTitle/PageTitle.tsx
+++ src/components/shared/PageTitle/PageTitle.tsx
@@ -1,7 +1,11 @@
 import { PropsWithChildren, ReactElement } from 'react'
 
 export default function PageTitle({ children, margin = 'fr-mt-5w' }: Props): ReactElement {
-  return <h1 className={`color-blue-france ${margin}`}>{children}</h1>
+  return (
+    <h1 className={`color-blue-france ${margin}`} style={{ fontSize: '2.25rem', lineHeight: '2.5rem' }}>
+      {children}
+    </h1>
+  )
 }
 
 type Props = PropsWithChildren<
